e9c6867dc962e0390f1adb4ca6b9819bd4a2ae39
Now100 <woqor10@snu.ac.kr>
2024-12-24 10:58:01 +0900

    Add various includes for analytics, embedding, and metadata handling; update configuration for improved functionality

--- _plugins/post_hook.rb
+++ _plugins/post_hook.rb
@@ -1,9 +1,8 @@
 Jekyll::Hooks.register :posts, :post_write do |post|
-    # 포스트 파일 이름 추출
-    filename = File.basename(post.path, ".*")
-    
-    # 이미지 경로 설정
-    img_dir = File.join("assets/img", filename)
-    FileUtils.mkdir_p(img_dir) unless File.directory?(img_dir)
-  end
+  # 포스트 파일 이름 추출
+  filename = File.basename(post.path, ".*")
   
+  # 이미지 경로 설정
+  img_dir = File.join("assets/img", filename)
+  FileUtils.mkdir_p(img_dir) unless File.directory?(img_dir)
+end
